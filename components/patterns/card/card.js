70486c0a3f5e84e5af1a61975e35aaaaf6ae1a52
jakubfiala <mu201jf@gold.ac.uk>
2017-08-24 12:35:35 +0100

    added fallback for non-js and old browsers for the flip card; fixed download attribute adding on buttons

--- components/patterns/card/card.js
+++ components/patterns/card/card.js
@@ -1,4 +1,8 @@
 const flipCard = cardWrapper => {
+  // do some preliminary feature checks
+  if (!('CSS' in window)) return;
+  if (!window.CSS.supports('transform', 'rotate3d(0,1,0,180deg)')) return;
+
   const tail = cardWrapper.querySelector('.card--tail');
   if (!tail) return;
 
