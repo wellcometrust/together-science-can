638a8f6ab4675ab29c8b3c3ddef40d6f2fb97c7b
jakubfiala <mu201jf@gold.ac.uk>
2017-08-29 16:49:54 +0100

    removed DS_Store exclude from the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
-aws s3 sync dist/ s3://togethersciencecan.org/ --region=eu-west-2 --delete --acl public-read --exclude ".DS_Store"
+aws s3 sync dist/ s3://togethersciencecan.org/ --region=eu-west-2 --delete --acl public-read
 # the aws command doesn't exit properly so we force-exit if successful
 # this tells CI we've successfully deployed
 exit 0
