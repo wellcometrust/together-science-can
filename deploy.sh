5f5f76bd20e22b160293534adc98da9735f19ab6
jakubfiala <mu201jf@gold.ac.uk>
2017-09-27 15:54:24 +0100

    invalidating CF cache on deploy for HTML

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 set -o errexit
+set -o nounset
 
 aws s3 sync dist/ s3://togethersciencecan.org/ --region=eu-west-2 --delete --acl public-read
+
+aws cloudfront create-invalidation \
+  --distribution-id "$AWS_CF_DISTRIBUTION_ID" \
+  --paths /index.html
+
 # the aws command doesn't exit properly so we force-exit if successful
 # this tells CI we've successfully deployed
 exit 0
