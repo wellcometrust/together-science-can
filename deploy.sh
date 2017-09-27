2aa6ed11d147548ff3efff23ef04ad7b117f95ba
jakubfiala <mu201jf@gold.ac.uk>
2017-09-27 15:58:45 +0100

    added step to enable cloudfront cli in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ set -o nounset
 
 aws s3 sync dist/ s3://togethersciencecan.org/ --region=eu-west-2 --delete --acl public-read
 
+# as of September 2017, Cloudfront CLI isn't enabled by default
+aws configure set preview.cloudfront true
+
 aws cloudfront create-invalidation \
   --distribution-id "$AWS_CF_DISTRIBUTION_ID" \
   --paths /index.html
