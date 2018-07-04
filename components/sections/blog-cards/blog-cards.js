2a4da7c510364be39bc6dedc9bd26c9d075f577b
shresthd <d.shrestha@wellcome.ac.uk>
2018-07-04 11:23:34 +0100

    dummy text removed.

--- components/sections/blog-cards/blog-cards.js
+++ components/sections/blog-cards/blog-cards.js
@@ -18,8 +18,8 @@ const loadMediumPosts = (container) => {
           const post = data[i];
           const date = new Date(post.date);
           node.querySelectorAll('img')[0].setAttribute('src', post.image_url);
-          node.querySelectorAll('.card__subtitle')[0].innerText = post.title || 'here is some text';
-          node.querySelectorAll('.card__body')[0].innerText = post.description || 'here is some text';
+          node.querySelectorAll('.card__subtitle')[0].innerText = post.title;
+          node.querySelectorAll('.card__body')[0].innerText = post.description;
           node.querySelectorAll('.card__footer')[0].innerText = monthNames[date.getMonth()] + ' ' + date.getDate();
           node.querySelectorAll('a')[0].href = post.post_url;
         });
